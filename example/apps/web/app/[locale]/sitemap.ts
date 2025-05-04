dbf736f9fe6a74ae95f455d96939e14ba173d900
Turbobot <turbobot@vercel.com>
2025-05-04 09:31:20 +0900

    feat(create-turbo): apply official-starter transform

---
+++ example/apps/web/app/[locale]/sitemap.ts
@@ -0,0 +1,27 @@
+import fs from 'node:fs';
+import { env } from '@/env';
+import type { MetadataRoute } from 'next';
+
+const appFolders = fs.readdirSync('app', { withFileTypes: true });
+const pages = appFolders
+  .filter((file) => file.isDirectory())
+  .filter((folder) => !folder.name.startsWith('_'))
+  .filter((folder) => !folder.name.startsWith('('))
+  .map((folder) => folder.name);
+const protocol = env.VERCEL_PROJECT_PRODUCTION_URL?.startsWith('https')
+  ? 'https'
+  : 'http';
+const url = new URL(`${protocol}://${env.VERCEL_PROJECT_PRODUCTION_URL}`);
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => [
+  {
+    url: new URL('/', url).href,
+    lastModified: new Date(),
+  },
+  ...pages.map((page) => ({
+    url: new URL(page, url).href,
+    lastModified: new Date(),
+  })),
+];
+
+export default sitemap;
---
+++ example/apps/web/next.config.ts
@@ -0,0 +1,33 @@
+import { env } from '@/env';
+import { config, withAnalyzer } from '@repo/next-config';
+import { withLogging, withSentry } from '@repo/observability/next-config';
+import type { NextConfig } from 'next';
+
+let nextConfig: NextConfig = withLogging(config);
+
+nextConfig.images?.remotePatterns?.push({
+  protocol: 'https',
+  hostname: 'assets.basehub.com',
+});
+
+if (process.env.NODE_ENV === 'production') {
+  const redirects: NextConfig['redirects'] = async () => [
+    {
+      source: '/legal',
+      destination: '/legal/privacy',
+      statusCode: 301,
+    },
+  ];
+
+  nextConfig.redirects = redirects;
+}
+
+if (env.VERCEL) {
+  nextConfig = withSentry(nextConfig);
+}
+
+if (env.ANALYZE === 'true') {
+  nextConfig = withAnalyzer(nextConfig);
+}
+
+export default nextConfig;
